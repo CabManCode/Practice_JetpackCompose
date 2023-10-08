0e514896ef277eddc221f86788c5a2b54c8966c8
CabManCode <dev@cabmancode.com>
2023-10-08 07:50:44 +0200

    Compose essentials - 04. Write your first Compose app - Basics Codelab - to be continued

--- ComposeEssentials/BasicsCodelab/app/src/main/java/com/cabmancode/dev/basicscodelab/MainActivity.kt
+++ ComposeEssentials/BasicsCodelab/app/src/main/java/com/cabmancode/dev/basicscodelab/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.cabmancode.dev.basicscodelab
 
+/***
+ * https://developer.android.com/codelabs/jetpack-compose-basics?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fjetpack-compose-for-android-developers-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-basics#6
+ */
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
